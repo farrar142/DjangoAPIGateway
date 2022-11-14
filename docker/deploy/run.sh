466bf6f5fba672958c429966af9be008a27df32c
farrar142 <farrar142@github.com>
2022-11-14 17:11:44 +0900

    fix:컨테이너 실행 ㅁ여령어 변경

--- docker/deploy/run.sh
+++ docker/deploy/run.sh
@@ -1,4 +1,4 @@
 # sh docker/celery.sh
 echo yes | python3 manage.py collectstatic
 python3 manage.py migrate --settings=base.settings
-uwsgi --env DJANGO_SETTINGS_MODULE=base.settings --ini uwsgi.local.ini
+uvicorn runthe_backend.asgi:application --port 8000 --host 0.0.0.0
