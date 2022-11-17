18644aabf08bca19474c19ace8127c93a375f1f5
farrar142 <farrar142@github.com>
2022-11-17 17:15:29 +0900

    fix:어드민 사이트에 업스트림도 추가

--- apigateway/admin.py
+++ apigateway/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Api, Consumer
+from .models import Api, Consumer,Upstream
 
 # Register your models here.
 admin.site.register(Api)
 admin.site.register(Consumer)
+admin.site.register(Upstream)
