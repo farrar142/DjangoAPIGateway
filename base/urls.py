6e0c6e317910d9aaf0e53314127dee60eaefb501
farrar142 <farrar142@github.com>
2022-11-17 17:36:11 +0900

    fix:어드민 url 변경

--- base/urls.py
+++ base/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include, re_path
 # from django.conf.urls import url
 from apigateway.views import gateway
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('gateway/', admin.site.urls),
     # path('test/', api.urls),
     re_path(r'.*', gateway.as_view()),
 ]
