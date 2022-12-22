fb945a9afa7933f61df1e2fee8c720d929c545e0
farrar142 <farrar142@github.com>
2022-12-22 15:37:25 +0900

    fix:원상복귀

--- docker/deploy/run.sh
+++ docker/deploy/run.sh
@@ -1,4 +1,4 @@
 # sh docker/celery.sh
 echo yes | python3 manage.py collectstatic
 python3 manage.py migrate --settings=base.settings
-uvicorn base.asgi:application --port 9999 --host 0.0.0.0 --workers 8
+uvicorn base.asgi:application --port 8000 --host 0.0.0.0 --workers 8
