96140495657a4a52f9a500a2bae461496536da5b
farrar142 <farrar142@github.com>
2022-12-30 12:51:26 +0900

    fix:관리자 페이지 정렬 순서 변경

--- apigateway/admin.py
+++ apigateway/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Api, Consumer,Upstream
+from .models import Api, Consumer, Upstream
+
+
+class APIAdmin(admin.ModelAdmin):
+    ordering = ("upstream", "request_path")
+
 
 # Register your models here.
-admin.site.register(Api)
+admin.site.register(Api, APIAdmin)
 admin.site.register(Consumer)
 admin.site.register(Upstream)
