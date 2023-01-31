c55add4016503ef8d4bafeaa8c18affad456092b
farrar142 <gksdjf1690@gmail.com>
2023-01-31 13:36:13 +0900

    fix:어드민 페이지 리스트 필터 추가

--- apigateway/admin.py
+++ apigateway/admin.py
@@ -7,6 +7,7 @@ from .models import Api, Consumer, Upstream
 
 class APIAdmin(admin.ModelAdmin):
     ordering = ("upstream", "request_path")
+    list_filter = ("upstream",)
 
 
 # Register your models here.
