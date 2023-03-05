4d0eeafdf547a28a4d078892b5d5b244fadaf0ea
farrar142 <gksdjf1690@gmail.com>
2023-03-06 04:31:10 +0900

    fix:어드민 페이지에 타겟 등록

--- apigateway/admin.py
+++ apigateway/admin.py
@@ -1,8 +1,12 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+
+from django.template.response import TemplateResponse
+from django.urls import path
+from django.shortcuts import redirect, get_object_or_404
 
 # Register your models here.
 
-from .models import Api, Consumer, Upstream
+from .models import Api, Consumer, Upstream, Target
 
 
 class APIAdmin(admin.ModelAdmin):
@@ -10,7 +14,64 @@ class APIAdmin(admin.ModelAdmin):
     list_filter = ("upstream", "plugin")
 
 
+class TargetInline(admin.TabularInline):
+    model = Target
+
+
+class UpstreamAdmin(admin.ModelAdmin):
+    inlines = [
+        TargetInline,
+    ]
+    readonly_fields = ("total_weight",)
+    fields = ("total_weight", "alias", "host", "scheme", "weight", "load_balance")
+
+    def get_queryset(self, request):
+        queryset = super(UpstreamAdmin, self).get_queryset(request)
+        queryset = queryset.prefetch_related("targets")
+        return queryset
+
+    def __init__(self, model, admin_site) -> None:
+        super().__init__(model, admin_site)
+
+
+class TargetAdmin(admin.ModelAdmin):
+    ordering = ("upstream",)
+    list_filter = ("upstream",)
+    list_display = ("__str__", "enabled", "toggle_button")
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path(
+                "<int:pk>/toggle-enabled",
+                self.toggle_enabled,
+                name="admin_toggle_enabled",
+            ),
+        ]
+        return my_urls + urls
+
+    def toggle_enabled(self, request, pk):
+        # ...
+        context = dict(
+            # Include common variables for rendering the admin template.
+            self.admin_site.each_context(request),
+            # Anything else you want in the context...
+        )
+
+        # Get the scenario to activate
+        target = get_object_or_404(Target, pk=pk)
+        # It is already activated
+        target.enabled = not target.enabled
+        target.save()
+        result = "Activated" if target.enabled else "Deactivated"
+        msg = f"{result} Target '{target}'"
+        self.message_user(request, msg, level=messages.INFO)
+        return redirect(request.META.get("HTTP_REFERER"))
+
+
 # Register your models here.
 admin.site.register(Api, APIAdmin)
 admin.site.register(Consumer)
-admin.site.register(Upstream)
+# admin.site.register(Upstream)
+admin.site.register(Upstream, UpstreamAdmin)
+admin.site.register(Target, TargetAdmin)
