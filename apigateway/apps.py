451770607a41949602742586ea6950cd590f9ac7
farrar142 <gksdjf1690@gmail.com>
2023-03-08 10:27:49 +0900

    fix:api캐싱을 단일 프로세스에서만 진행하도록

--- apigateway/apps.py
+++ apigateway/apps.py
@@ -1,17 +1,24 @@
 from threading import Thread
 from django.apps import AppConfig
+from common_module.caches import cache
 from common_module.caches import UseSingleCache
 
 
 def warm_cache():
     from .models import Api
 
+    is_running = cache.get("warm_up", False)
+    if is_running:
+        print("캐싱작업 진행중 태스크를 종료합니다")
+        return
+    cache.set("warm_up", True)
     api_cache = UseSingleCache(0, "api")
     for api in Api.objects.prefetch_related("upstream", "upstream__targets").iterator():
         print("set", api)
         api_cache.set(
             api, 3600 * 24 * 30, path=api.request_path, upstream=api.upstream.pk
         )
+    cache.delete("warm_up")
 
 
 class ApigatewayConfig(AppConfig):
