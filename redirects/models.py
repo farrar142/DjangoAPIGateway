e6eff5347848c204038dbfc6d962d29e39738596
farrar142 <gksdjf1690@gmail.com>
2023-03-17 13:10:40 +0900

    fix:수정된 모델 반영

--- redirects/models.py
+++ redirects/models.py
@@ -4,5 +4,3 @@ from django.db import models
 class RedirectionPage(models.Model):
     requested_path = models.CharField(max_length=512)
     target_path = models.URLField()
-class TestModel(models.Model):
-    name = models.CharField(max_length=32)
